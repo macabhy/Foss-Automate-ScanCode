ce6f7b2f947a832d5eef23c57fc82c437d10a7b6
macabhy <abhaykumar754@gmail.com>
2025-08-07 11:36:11 +0530

    Removed GPL license header from index.ts

--- src/uuid/src/index.ts
+++ src/uuid/src/index.ts
@@ -1,9 +1,5 @@
-/*
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- */
+// This file is part of the UUID library.
+// SPDX-License-Identifier: MIT
 export type * from './types.js';
 export { default as MAX } from './max.js';
 export { default as NIL } from './nil.js';
